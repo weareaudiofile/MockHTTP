329e90f1cbdd475348b5999213f5bb857a8b799a
Rachel Brindle <you@subluminal.net>
2015-03-31 21:36:58 -0700

    Initial Commit

---
+++ MockHTTP/MockHTTP.h
@@ -0,0 +1,19 @@
+//
+//  MockHTTP.h
+//  MockHTTP
+//
+//  Created by Rachel Brindle on 3/31/15.
+//  Copyright (c) 2015 Rachel Brindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MockHTTP.
+FOUNDATION_EXPORT double MockHTTPVersionNumber;
+
+//! Project version string for MockHTTP.
+FOUNDATION_EXPORT const unsigned char MockHTTPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MockHTTP/PublicHeader.h>
+
+
